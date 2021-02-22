e9df5d7664082cef2ce95d519e51fe9b7399e1b9
Gaulthier Gain <gaulthier.gain@uliege.be>
2021-02-22 10:05:07 +0100

    Adding resources for students
    Signed-off-by: Gaulthier Gain <gaulthier.gain@uliege.be>

---
+++ tracee.c
@@ -0,0 +1,12 @@
+/* This file represents a sample tracee program
+ * that must be compiled as follow:
+ *      gcc --static -g -o tracee tracee.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("hello.c");
+    return 1;
+}
